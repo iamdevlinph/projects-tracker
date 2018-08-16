893077168c7754117b94bf1fd5f93a3d97169cfc
iamdevlinph <iamdevlinph@gmail.com>
2018-08-16 11:56:27 +0800

    Disable close on outside click

--- src/routes/HomeLayout/HomeLayout.js
+++ src/routes/HomeLayout/HomeLayout.js
@@ -33,6 +33,7 @@ class HomeLayout extends Component {
         text: 'Other users currently don\'t have any functions right now',
         type: 'info',
         footer: '<a href="https://github.com/iamdevlinph/projects-tracker/issues/13" target="blank">Read more</a>',
+        allowOutsideClick: false,
       });
       localStorage.setItem('informedUserCache', { flag: true });
     }
