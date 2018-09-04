19fd3ce5e8a64b95284e029f5e6307942048f462
iamdevlinph <iamdevlinph@gmail.com>
2018-09-04 16:52:47 +0800

    Change component to stateless

--- src/components/FilterButton/FilterButton.js
+++ src/components/FilterButton/FilterButton.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '../Button/Button';
 
-class FilterButtonComponent extends Component {
-  render() {
-    const {
-      label, disabled, sortFunc, value, sort,
-    } = this.props;
-    const click = () => {
-      const orderByAsc = sort.field === value ? !sort.isAsc : true;
-      sortFunc(value, orderByAsc);
-    };
-    const asc = sort.isAsc ? 'fas fa-caret-up' : 'fas fa-caret-down';
-    const caretDirection = sort.field === value ? asc : 'down';
-    return (
-      <Button
-        onClick={click}
-        label={label}
-        icon={<i className={caretDirection} />}
-        disabled={disabled}
-      />
-    );
-  }
-}
+const FilterButtonComponent = (props) => {
+  const {
+    label, disabled, sortFunc, value, sort,
+  } = props;
+  const click = () => {
+    const orderByAsc = sort.field === value ? !sort.isAsc : true;
+    sortFunc(value, orderByAsc);
+  };
+  const asc = sort.isAsc ? 'fas fa-caret-up' : 'fas fa-caret-down';
+  const caretDirection = sort.field === value ? asc : 'down';
+  return (
+    <Button
+      onClick={click}
+      label={label}
+      icon={<i className={caretDirection} />}
+      disabled={disabled}
+    />
+  );
+};
 
 FilterButtonComponent.propTypes = {
   label: PropTypes.string.isRequired,
