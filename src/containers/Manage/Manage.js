2912e469d81c87a2cb948fff02d09df20f8b35a1
iamdevlinph <iamdevlinph@gmail.com>
2018-10-08 19:12:29 +0800

    Manage page add repo ui

--- src/containers/Manage/Manage.js
+++ src/containers/Manage/Manage.js
@@ -2,21 +2,51 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { ProjectsTbl } from '../../components';
+import { ProjectsTbl, Button } from '../../components';
 
 class ManageContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authorName: '',
+      repoName: '',
+    };
+  }
+
+  handleInputChange = (e, field) => {
+    this.setState({ [field]: e.target.value });
+  }
+
   render() {
     const {
-      projects, loggedIn, deleteProject,
+      projects, loggedIn, deleteProject, saveProject,
     } = this.props;
     const options = {
       showPagination: false,
       pageSize: projects.length,
     };
+    const { authorName, repoName } = this.state;
     return (
       <ManageArea>
         <AddProject>
-          Add Project
+          <h3>Add Repository</h3>
+          <span>Author Name</span>
+          <input
+            type="text"
+            onChange={e => this.handleInputChange(e, 'authorName')}
+          />
+          <span>Repository Name</span>
+          <input
+            type="text"
+            onChange={e => this.handleInputChange(e, 'repoName')}
+          />
+          <Button
+            onClick={() => saveProject(authorName, repoName)}
+            label="Save"
+            icon={<i className="fas fa-plus" />}
+            color="08AF4C"
+            disabled={authorName === '' || repoName === ''}
+          />
         </AddProject>
         <ProjectsList>
           <ProjectsTbl
@@ -34,6 +64,7 @@ class ManageContainer extends Component {
 ManageContainer.propTypes = {
   projects: PropTypes.array,
   deleteProject: PropTypes.func.isRequired,
+  saveProject: PropTypes.func.isRequired,
   loggedIn: PropTypes.bool,
 };
 
@@ -52,10 +83,14 @@ const ManageArea = styled.div`
     "manage-top-pad manage-top-pad"
     "add-project project-list";
   row-gap: 10px;
+  column-gap: 20px;
 `;
 const ProjectsList = styled.div`
   grid-area: project-list;
 `;
 const AddProject = styled.div`
   grid-area: add-project;
+  display: grid;
+  grid-template-rows: repeat(5, min-content);
+  row-gap: 10px;
 `;
